1ec15686310945ab8baaba40179e0ab72cef8ec3
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-07-28 19:05:22 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -55,7 +55,7 @@ export default function AwardApp({ Component, pageProps }) {
     },
     {
       link: true,
-      name: "Partnerler",
+      name: "Partner ve Sponsorlar",
       icon: "fal fa-handshake",
       activeIcon: "fa fa-handshake",
       href: "/partners",
