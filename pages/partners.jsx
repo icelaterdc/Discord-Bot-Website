28ad551460b23a46841e3260cb03907f21dfe0de
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-07-28 19:14:57 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,9 +18,9 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://codeshare.me">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1262067722502672384/1267149606089654434/IMG_20240728_185842_100_1.jpg?ex=66a7bc88&is=66a66b08&hm=f37f32df06556dcd5c0697eb4a35aca6811d21e403e4130f8135c7dc2e8d5a54&.png" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1262067722502672384/1267152930788741140/IMG_20240728_191246_001.jpg?ex=66a7bfa1&is=66a66e21&hm=e699724aed0462d0341463c57798dda3e53a5521641aec4777fc090e6013e4d1&.png" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Oksit Web</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                       Ucuz, Kalitleli ve Güvenilir Hostingin Adresi OksitWeb! Sunucularda Kesinti Olmaksızın 7/24 Hizmet Vermektedir.
