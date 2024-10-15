2cbca53a3fa67063e5adc4609e35d3a4274a7ec7
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-10-15 20:00:20 +0300

    Delete api.jsx

--- api.jsx
+++
@@ -1,8 +0,0 @@
-export default function ApiDocumentation() {
-  return (
-    <div>
-      <h1>API Dokümantasyonu</h1>
-      <p>Burada API ile ilgili dokümantasyon bulabilirsiniz.</p>
-    </div>
-  );
-}
