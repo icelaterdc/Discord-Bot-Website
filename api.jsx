a756955132caeec013a1f4accbdf2685d69a17bd
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-10-15 19:03:28 +0300

    Create api.jsx

---
+++ api.jsx
@@ -0,0 +1,8 @@
+export default function ApiDocumentation() {
+  return (
+    <div>
+      <h1>API Dokümantasyonu</h1>
+      <p>Burada API ile ilgili dokümantasyon bulabilirsiniz.</p>
+    </div>
+  );
+}
