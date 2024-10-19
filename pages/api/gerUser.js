daf396551fa0b84f0ce5cc775d57a066f00e71da
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-10-19 23:04:44 +0300

    Delete pages/api/gerUser.js

--- pages/api/gerUser.js
+++
@@ -1,9 +0,0 @@
-// pages/api/getUser.js
-
-export default async function getUser(req, res) {
-  const user = req.session.user; // Giriş yapan kullanıcı bilgilerini session'dan al
-  if (!user) {
-    return res.status(401).json({ error: "Lütfen giriş yapın." });
-  }
-  res.status(200).json(user);
-}
