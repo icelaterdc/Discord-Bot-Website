c3258424e4044478b4ec4e35c072f3ee3003dccd
Mamecan211 <120577848+Mamecan211@users.noreply.github.com>
2024-10-19 23:44:22 +0300

    Update bot-guilds.js

--- pages/api/bot-guilds.js
+++ pages/api/bot-guilds.js
@@ -1,15 +1,22 @@
 // pages/api/bot-guilds.js
-import { getBot } from '../../lib/discord-bot'; // Bot client'ınıza erişim sağlayan bir fonksiyon
+import axios from 'axios';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const bot = getBot();
-      const guilds = bot.guilds.cache.map(guild => ({
+      const botToken = 'MTIwMTYxMzY2NzU2MTYzOTk0Nw.GeJ7lr.BkGxlyx_xnV-pWcLlcW0COBOme3HUDQgfIn2Pc'; // Bot token'ı burada açık bir şekilde verilmiştir.
+      const response = await axios.get('https://discord.com/api/users/@me/guilds', {
+        headers: {
+          Authorization: `Bot ${botToken}`,
+        },
+      });
+
+      const guilds = response.data.map(guild => ({
         id: guild.id,
         name: guild.name,
         icon: guild.icon
       }));
+
       res.status(200).json(guilds);
     } catch (error) {
       console.error('Failed to fetch bot guilds:', error);
